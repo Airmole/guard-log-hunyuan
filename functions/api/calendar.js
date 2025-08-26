6082019507c9fc20b8c7a3954580a811d8a388f2
Airmole <admin@airmole.cn>
2025-08-26 15:50:39 +0800

    Update calendar.js

--- functions/api/calendar.js
+++ functions/api/calendar.js
@@ -2,27 +2,29 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   const queryParams = Object.fromEntries(url.searchParams.entries());
 
-  let year;
-  if (queryParams.year) {
-    year = queryParams.year;
+  let month;
+  if (queryParams.month) {
+    month = queryParams.month;
   } else {
-    year = new Date().getFullYear();
+    const year = new Date().getFullYear();
+    month = new Date().getMonth() + 1;
+    if (month < 10) month = '0' + month;
+    month = `${year}-${month}`
   }
 
- 
   let clendarData = [];
   let result;
   let cursor;
   do {
-      result = await guardlog_kv.list({ "prefix": `clendar_${year}`, "limit": 200 });
-      cursor = result.cursor;
-      for (let index = 0; index < result.keys.length; index++) {
-          const keyname = result.keys[index].key;
-          const value = await guardlog_kv.get(keyname, 'json');
-          clendarData.push(value);
-      }
+    result = await guardlog_kv.list({ "prefix": `clendar_${month}`, "limit": 40 });
+    cursor = result.cursor;
+    for (let index = 0; index < result.keys.length; index++) {
+      const keyname = result.keys[index].key;
+      const value = await guardlog_kv.get(keyname, 'json');
+      clendarData.push(value);
+    }
   } while (result && !result.complete);
-  
+
   return new Response(JSON.stringify(clendarData), {
     headers: {
       "Access-Control-Allow-Origin": "*",
